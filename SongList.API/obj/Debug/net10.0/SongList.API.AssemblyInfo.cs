cef995d30dd63eee317c60075d9ba9713dcee9ba
HangerIssues <jakesmith01@zoominternet.net>
2026-08-08 19:22:44 -0400

    Refactored menu and added initial test project

--- SongList.API/obj/Debug/net10.0/SongList.API.AssemblyInfo.cs
+++ SongList.API/obj/Debug/net10.0/SongList.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SongList.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ff3ac55afb0f701f97e75ec09e990a6bfdd53f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e96e9ffb3d8cef58e972616356929d9b34e97f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("SongList.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SongList.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SongList.Console/Program.cs
+++ SongList.Console/Program.cs
@@ -4,9 +4,9 @@ using System.Text;
 using System.Text.Json;
 using SongList.Console.Models;
 
-HttpClient client = new HttpClient();
-client.BaseAddress = new Uri("http://localhost:5298");
-HttpResponseMessage response;
+//HttpClient client = new HttpClient();
+//client.BaseAddress = new Uri("http://localhost:5298");
+//HttpResponseMessage response;
 
 bool exit = false;
 while (!exit)
@@ -24,7 +24,7 @@ while (!exit)
     switch (choice)
     {
         case "1":
-            response = await client.GetAsync("/songs");
+            /*response = await client.GetAsync("/songs");
             if(response.IsSuccessStatusCode)
             {
                 var songsRaw = await response.Content.ReadAsStringAsync();
@@ -43,10 +43,20 @@ while (!exit)
             else
             {
                 Console.WriteLine($"Error: {response.StatusCode}");
+            }*/
+            var songs = await GetAllSongs();
+
+            foreach(var song in songs)
+            {
+                Console.WriteLine($"Id: {song.Id}");
+                Console.WriteLine($"Title: {song.Title}");
+                Console.WriteLine($"Artist: {song.Artist}");
+                Console.WriteLine();
             }
+
             break;
         case "2":
-            Console.WriteLine("Enter search Id: ");
+            /*Console.WriteLine("Enter search Id: ");
             string? searchIdString = Console.ReadLine();
             if(searchIdString != null)
             {
@@ -78,10 +88,23 @@ while (!exit)
             else
             {
                 Console.WriteLine("Invalid choice.");
-            }
+            }*/
+            var searchSong = await GetSongById();
+
+            string tagsString = string.Join(",", searchSong.Tags);
+            
+            Console.WriteLine($"Id: {searchSong.Id}");
+            Console.WriteLine($"Title: {searchSong.Title}");
+            Console.WriteLine($"Artist: {searchSong.Artist}");
+            Console.WriteLine($"Album: {searchSong.Album}");
+            Console.WriteLine($"Year: {searchSong.Year}");
+            Console.WriteLine($"Genre: {searchSong.Genre}");
+            Console.WriteLine($"Tags: {tagsString}");
+            Console.WriteLine();
+
             break;
         case "3":
-            Console.WriteLine("Enter new song information.");
+            /*Console.WriteLine("Enter new song information.");
 
             //Console.WriteLine("Id: ");
             //int newId = int.Parse(Console.ReadLine());
@@ -131,11 +154,12 @@ while (!exit)
             else
             {
                 Console.WriteLine($"Error: {response.StatusCode}");
-            }
+            }*/
+            await AddNewSong();
 
             break;
         case "4":
-            Console.WriteLine("Enter id of song to update: ");
+            /*Console.WriteLine("Enter id of song to update: ");
             string? updateIdString = Console.ReadLine();
             if(updateIdString != null)
             {
@@ -213,11 +237,12 @@ while (!exit)
             else
             {
                 Console.WriteLine("Invalid choice");
-            }
+            }*/
+            await UpdateExistingSong();
 
             break;
         case "5":
-            Console.WriteLine("Enter id of song to be deleted: ");
+            /*Console.WriteLine("Enter id of song to be deleted: ");
             string? deleteIdString = Console.ReadLine();
             if(deleteIdString != null)
             {
@@ -231,7 +256,8 @@ while (!exit)
                 {
                     Console.WriteLine($"Error: {response.StatusCode}");
                 }
-            }
+            }*/
+            await DeleteSong();
             break;
         case "6":
             exit = true;
@@ -242,8 +268,8 @@ while (!exit)
             break;
     }
 }
-/*
-async void ListAllSongs()
+
+async Task<List<Song>> GetAllSongs()
 {
     using (var client = new HttpClient())
     {
@@ -251,39 +277,231 @@ async void ListAllSongs()
         HttpResponseMessage response = await client.GetAsync("/songs");
         if(response.IsSuccessStatusCode)
         {
-            var data = await response.Content.ReadAsStringAsync();
-            Console.WriteLine(data);
-            return;
+            var songsRaw = await response.Content.ReadAsStringAsync();
+            var songs = JsonSerializer.Deserialize<List<Song>>(
+                songsRaw,
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            
+            /*foreach(var song in songs)
+            {
+                Console.WriteLine($"Id: {song.Id}");
+                Console.WriteLine($"Title: {song.Title}");
+                Console.WriteLine($"Artist: {song.Artist}");
+                Console.WriteLine();
+            }*/
+
+            if(songs != null)
+            {
+                return songs;
+            }
+            else
+            {
+                Console.WriteLine("No Songs Found.");
+                return new List<Song>();
+            }
         }
         else
         {
             Console.WriteLine($"Error: {response.StatusCode}");
+            return new List<Song>();
+        }
+    }
+}
+
+async Task<Song> GetSongById()
+{
+    using (var client = new HttpClient())
+    {
+        client.BaseAddress = new Uri("http://localhost:5298");
+        Console.WriteLine("Enter search Id: ");
+        string? searchIdString = Console.ReadLine();
+        if (int.TryParse(searchIdString, out int searchId))
+        {
+            HttpResponseMessage response = await client.GetAsync($"/songs/{searchId}");
+            if(response.IsSuccessStatusCode)
+            {
+                var searchSongRaw = await response.Content.ReadAsStringAsync();
+                var searchSong = JsonSerializer.Deserialize<Song>(
+                    searchSongRaw,
+                    new JsonSerializerOptions { PropertyNameCaseInsensitive = true});
+                
+                if(searchSong != null)
+                {
+                    return searchSong;
+                }
+                else
+                {
+                    Console.WriteLine($"Song with id {searchId} not found.");
+                    var songs = await GetAllSongs();
+                    Console.WriteLine($"Id to search cannot be less than 1 or greater than {songs.Count}.");
+                    return await GetSongById();
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Error: {response.StatusCode}");
+                return await GetSongById();
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid response. Please enter a number.");
+            return await GetSongById();
+        }
+    }
+}
+
+async Task AddNewSong()
+{
+    using (var client = new HttpClient())
+    {
+        client.BaseAddress = new Uri("http://localhost:5298");
+        var songList = await GetAllSongs();
+        Console.WriteLine("Enter new song information.");
+        Console.WriteLine("Title: ");
+        string? newTitle = Console.ReadLine();
+
+        Console.WriteLine("Artist: ");
+        string? newArtist = Console.ReadLine();
+        
+        Console.WriteLine("Album: ");
+        string? newAlbum = Console.ReadLine();
+
+        Console.WriteLine("Year: ");
+        string? newYearString = Console.ReadLine();
+
+        Console.WriteLine("Genre: ");
+        string? newGenre = Console.ReadLine();
+
+        Console.WriteLine("Tags (Seperate by commas): ");
+        string? newTagsString = Console.ReadLine();
+
+        if(string.IsNullOrWhiteSpace(newTitle) || string.IsNullOrWhiteSpace(newArtist) || string.IsNullOrWhiteSpace(newAlbum) || string.IsNullOrWhiteSpace(newYearString) || string.IsNullOrWhiteSpace(newGenre) || string.IsNullOrWhiteSpace(newTagsString))
+        {
+            Console.WriteLine("Please provide all fields.");
+            await AddNewSong();
+            return;
+        }
+        else if(int.TryParse(newYearString, out int newYear))
+        {
+            List<string> newTags = newTagsString.Split(",").ToList();
+            Song newSong = new Song
+            {
+                Id = songList.Count + 1,
+                Title = newTitle,
+                Artist = newArtist,
+                Album = newAlbum,
+                Year = newYear,
+                Genre = newGenre,
+                Tags = newTags
+            };
+
+            string newSongRaw = JsonSerializer.Serialize(newSong);
+            var stringContent = new StringContent(newSongRaw, Encoding.UTF8, "application/json");
+            await client.PostAsync("/songs", stringContent);
+            Console.WriteLine($"Created song with id: {newSong.Id}");
+            return;
+        }
+        else
+        {
+            Console.WriteLine("Please enter valid year.");
+            await AddNewSong();
             return;
         }
     }
 }
 
-List<Song> SearchSongList()
+async Task UpdateExistingSong()
 {
-    // Implement logic to search the song list
-    Console.WriteLine("Searching song list...");
-    return new List<Song>();
+    using (var client = new HttpClient())
+    {
+        client.BaseAddress = new Uri("http://localhost:5298");
+        var updateSong = await GetSongById();
+
+        Console.WriteLine("Enter new information. Leave blank to keep existing information.");
+        Console.WriteLine($"Current Title: {updateSong.Title}");
+        Console.WriteLine("New Title: ");
+        string? updateTitle = Console.ReadLine();
+        if(!string.IsNullOrWhiteSpace(updateTitle))
+        {
+            updateSong.Title = updateTitle;
+        }
+
+        Console.WriteLine($"Current Artist: {updateSong.Artist}");
+        Console.WriteLine("New Artist: ");
+        string? updateArtist = Console.ReadLine();
+        if(!string.IsNullOrWhiteSpace(updateArtist))
+        {
+            updateSong.Artist = updateArtist;
+        }
+
+        Console.WriteLine($"Current Album: {updateSong.Album}");
+        Console.WriteLine("New Album: ");
+        string? updateAlbum = Console.ReadLine();
+        if(!string.IsNullOrWhiteSpace(updateAlbum))
+        {
+            updateSong.Album = updateAlbum;
+        }
+
+        Console.WriteLine($"Current Year: {updateSong.Year}");
+        Console.WriteLine("New Year: ");
+        string? updateYearString = Console.ReadLine();
+        if(int.TryParse(updateYearString, out int updateYear))
+        {
+            updateSong.Year = updateYear;
+        }
+
+        Console.WriteLine($"Current Genre: {updateSong.Genre}");
+        Console.WriteLine("New Genre: ");
+        string? updateGenre = Console.ReadLine();
+        if (!string.IsNullOrWhiteSpace(updateGenre))
+        {
+            updateSong.Genre = updateGenre;
+        }
+
+        string currentTagsString = string.Join(",", updateSong.Tags);
+        Console.WriteLine($"Current Tags: {currentTagsString}");
+        Console.WriteLine("New Tags (Seperate by commas): ");
+        string? updateTagsString = Console.ReadLine();
+        if(!string.IsNullOrWhiteSpace(updateTagsString))
+        {
+            List<string> updateTags = updateTagsString.Split(",").ToList();
+            updateSong.Tags = updateTags;
+        }
+
+        string updateJson = JsonSerializer.Serialize(updateSong);
+        var updateStringContent = new StringContent(updateJson, Encoding.UTF8, "application/json");
+        await client.PutAsync($"/songs/{updateSong.Id}", updateStringContent);
+        Console.WriteLine($"Updated song with id: {updateSong.Id}");
+        return;
+    }
 }
 
-void AddNewSong()
+async Task DeleteSong()
 {
-    // Implement logic to add a new song
-    Console.WriteLine("Adding a new song...");
+    using (var client = new HttpClient())
+    {
+        client.BaseAddress = new Uri("http://localhost:5298");
+        Console.WriteLine("Enter id of song to be deleted: ");
+        string? deleteIdString = Console.ReadLine();
+        if(int.TryParse(deleteIdString, out int deleteId))
+        {
+            HttpResponseMessage response = await client.DeleteAsync($"/songs/{deleteId}");
+            if(response.IsSuccessStatusCode)
+            {
+                Console.WriteLine($"Deleted song with Id: {deleteId}");
+                return;
+            }
+            else
+            {
+                Console.WriteLine($"Error: {response.StatusCode}");
+                return;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid Id.");
+            await DeleteSong();
+        }
+    }
 }
-
-void UpdateExistingSong()
-{
-    // Implement logic to update an existing song
-    Console.WriteLine("Updating an existing song...");
-}
-
-void DeleteSong()
-{
-    // Implement logic to delete a song
-    Console.WriteLine("Deleting a song...");
-}*/
